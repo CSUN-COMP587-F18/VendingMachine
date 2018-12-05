1300ff0497abaf2b002644c23998a94e7130613e
Michael Shi <michael.shi.596@my.csun.edu>
2018-12-04 21:14:13 -0800

    11th Pull Request - Automated testing

--- VendingMachineTester/GoodCoin.cs
+++ VendingMachineTester/GoodCoin.cs
@@ -75,6 +75,84 @@ namespace VendingMachineTester
             CheckUnload(contents, 190, 0, new string[] { "Coke" });
         }
 
+        /// <summary>
+        /// UT04-GC
+        /// Automated testing for exact change
+        /// </summary>
+        [TestMethod]
+        public void GoodAutoExactChange()
+        {
+            Random rnd = new Random();
+
+            // Configure vending machine to have enough pop and coins for the tests
+            var vm = new VendingMachine(new int[] { 5, 10, 25, 100 }, 3, 100000, 100000, 100000);
+            new VendingMachineLogic(vm);
+            vm.Configure(new List<string>() { "Coke", "Water", "Juice" }, new List<int>() { 250, 200, 150 });
+            vm.LoadCoins(new int[] { 50000, 50000, 50000, 50000 });
+            vm.LoadPopCans(new int[] { 10000, 10000, 10000 });
+
+            int i = 1001;
+            while (i > 0)
+            {
+                // Randomly selecting the coins for credit (enough to purchase)
+                int credit = 0;
+                while (credit <= 250)
+                {
+                    int num = rnd.Next(1, 5);
+                    if (num == 1)
+                    {
+                        vm.CoinSlot.AddCoin(new Coin(5));
+                        credit += 5;
+                    }
+                    if (num == 2)
+                    {
+                        vm.CoinSlot.AddCoin(new Coin(10));
+                        credit += 10;
+                    }
+                    if (num == 3)
+                    {
+                        vm.CoinSlot.AddCoin(new Coin(25));
+                        credit += 25;
+                    }
+                    if (num == 4)
+                    {
+                        vm.CoinSlot.AddCoin(new Coin(100));
+                        credit += 100;
+                    }
+                }
+
+                // Random choice
+                string choice = "";
+                int cost = 0;
+                int select = rnd.Next(0, 3);
+                if (select == 0)
+                {
+                    vm.SelectionButtons[0].Press();
+                    cost = 250;
+                    choice = "Coke";
+                }
+                if (select == 1)
+                {
+                    vm.SelectionButtons[1].Press();
+                    cost = 200;
+                    choice = "Water";
+                }
+                if (select == 2)
+                {
+                    vm.SelectionButtons[2].Press();
+                    cost = 150;
+                    choice = "Juice";
+                }
+
+                // Check if test passes
+                credit -= cost;
+                var delivery = VMUtility.ExtractDelivery(vm);
+                CheckDelivery(delivery, credit, new string[] { choice });
+                i--;
+            }
+
+        }
+
         //Check delivery chute contents
         public void CheckDelivery(List<IDeliverable> delivery, int cents, string[] popNames)
         {
